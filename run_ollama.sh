941760bef0bfeef16d0e1c5f1e6b6c435796eb96
pszajows <pszajow@gmail.com>
2024-01-30 10:50:13 +0100

    add content

---
+++ run_ollama.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#OLLAMA_HOST=127.0.0.1:11434 ollama run deepseek-coder:latest
+OLLAMA_HOST=127.0.0.1:11434 ollama run dolphin-phi:latest
+#OLLAMA_HOST=127.0.0.1:11434 ollama run orca-mini:latest
+#OLLAMA_HOST=127.0.0.1:11434 ollama run phi:latest
+#OLLAMA_HOST=127.0.0.1:11434 ollama run stablelm-zephyr:latest
+#OLLAMA_HOST=127.0.0.1:11434 ollama run starcoder:latest
+#OLLAMA_HOST=127.0.0.1:11434 ollama run tinyllama:latest
---
+++ start_ollama.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+OLLAMA_HOST=0.0.0.0 OLLAMA_ORIGINS=* ollama serve > /dev/null
+
